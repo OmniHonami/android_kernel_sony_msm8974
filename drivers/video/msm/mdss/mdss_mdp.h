660c51b3d5e14a4b9750cb63a9290ba69e1709ce
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2014-02-01 10:56:03 +0100

    Revert "msm: mdss: Program phase factors for QSEED in Dual display mode"
    This reverts commit 876d118074771dd3b1d4ddf1e2436d772548bd71.
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_mdp.h
    
    Change-Id: I3729053c3de50ddf402a03ca9e20734178aea08b

--- drivers/video/msm/mdss/mdss_mdp.h
+++ drivers/video/msm/mdss/mdss_mdp.h
@@ -359,6 +359,9 @@ struct mdss_mdp_pipe {
 	u8 vert_deci;
 	struct mdss_mdp_img_rect src;
 	struct mdss_mdp_img_rect dst;
+	u32 phase_step_x;
+	u32 phase_step_y;
+
 	struct mdss_mdp_format_params *src_fmt;
 	struct mdss_mdp_plane_sizes src_planes;
 
@@ -387,9 +390,6 @@ struct mdss_mdp_pipe {
 
 	struct mdp_overlay_pp_params pp_cfg;
 	struct mdss_pipe_pp_res pp_res;
-	struct mdp_scale_data scale;
-	u8 chroma_sample_h;
-	u8 chroma_sample_v;
 };
 
 struct mdss_mdp_writeback_arg {
@@ -662,7 +662,6 @@ int mdss_mdp_wb_set_format(struct msm_fb_data_type *mfd, int dst_format);
 int mdss_mdp_wb_get_format(struct msm_fb_data_type *mfd,
 					struct mdp_mixer_cfg *mixer_cfg);
 
-int mdss_mdp_pipe_program_pixel_extn(struct mdss_mdp_pipe *pipe);
 int mdss_mdp_wb_set_secure(struct msm_fb_data_type *mfd, int enable);
 int mdss_mdp_wb_get_secure(struct msm_fb_data_type *mfd, uint8_t *enable);
 
